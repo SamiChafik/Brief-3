bb0975263d027b15cf8b6ceb1e3f1c78332683f3
SamiChafik <sami.chafik00@gmail.com>
2024-12-27 12:11:41 +0100

    class for colors

---
+++ Colors.java
@@ -0,0 +1,14 @@
+public class Colors {
+        String bold = "\u001b[1m";
+        String reset = "\u001b[0m";
+        String under = "\u001b[4m";
+        String black = "\u001B[30m";
+        String red = "\033[0;91m";
+        String green = "\033[0;92m";
+        String yellow = "\033[0;93m";
+        String blue = "\033[0;94m";
+        String magenta = "\033[0;95m";
+        String cyan = "\u001B[36m";
+        String grey = "\u001B[37m";
+        String white = "\u001B[38m";
+}
